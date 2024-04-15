eb15c6c94cee5d54d7bee957ce1dd3c6eb718f71
Rembqq <nikitavolkovskyi@gmail.com>
2024-04-15 17:10:42 +0300

    build structure

---
+++ src/test/java/com/example/lab03starterprojectjava/MainTests.java
@@ -0,0 +1,13 @@
+package com.example.lab03starterprojectjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
